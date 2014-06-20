afc2c85f84894eb443d5a1d8806bb5bf4ebbba41
BROLYMP <mbhsnikitak@gmail.com>
2014-06-20 00:35:25 -0400

    try 3

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -27,45 +27,80 @@ function gradeAll(){
     }
 }
 function gradeEm(value){
-var kdaId = value + '_kda';
-var gradeId = value + '_grade';
+var kdaId = value + '_kda'; //айди элемента кда
+var goldId = value + '_gold'; //айди голды
+var gpmId = value + '_gpm'; // айди гпм
+var killsId = value + '_kills'; // айди килов
+var deathsId = value + '_deaths'; // айди смертей 
+var assistsId = value + '_assists' // айди ассистов
+var csId = value + '_cs' //айди ластхитов
+
+var gradeId = value + '_grade'; //айди элемента оценки
+var gold_g = value + '_gold_g'; //айди оценики голды
+var gpm_g = value + '_gpm_g'; // айди оценки гпм
+var kills_g = value + '_kills_g'; // айди оценки килов
+var deaths_g = value + '_deaths_g'; // айди оценки смертей 
+var assists_g = value + '_assists_g' // айди оценки ассистов
+var cs_g = value + '_cs_g' //айди оценки ластхитов
+
 var kda = parseFloat(document.getElementById(kdaId).innerHTML);
+var gold = parseFloat(document.getElementById(goldId).innerHTML);
+var gpm = parseFloat(document.getElementById(gpmId).innerHTML);
+var kills = parseFloat(document.getElementById(killsId).innerHTML);
+var deaths = parseFloat(document.getElementById(deathsId).innerHTML);
+var assists = parseFloat(document.getElementById(assistsId).innerHTML);
+var cs = parseFloat(document.getElementById(csId).innerHTML);
+
+
+//анализируем кда
+
 if (kda>=8&&value!='SUP'&&value!='JUN'){
 document.getElementById(gradeId).innerHTML = 'A+';
-document.getElementById(gradeId).style.color='gold';
+document.getElementById(gradeId).style.color='#660066';
 }
 else if(kda>=6&&kda<8&&value!='SUP'&&value!='JUN'){
 document.getElementById(gradeId).innerHTML = 'A';
+document.getElementById(gradeId).style.color='green';
 }
 else if(kda>=4.5&&kda<6&&value!='SUP'&&value!='JUN'){
 document.getElementById(gradeId).innerHTML = 'A-';
+document.getElementById(gradeId).style.color='gold';
 }
 else if(kda>=3&&kda<4.5&&value!='SUP'&&value!='JUN'){
 document.getElementById(gradeId).innerHTML = 'B';
-document.getElementById(gradeId).style.color='blue';
+document.getElementById(gradeId).style.color='grey'; 
 }
 else if(kda<3&&value!='SUP'&&value!='JUN'){
 document.getElementById(gradeId).innerHTML = 'C';
-document.getElementById(gradeId).style.color='bronze'; 
+document.getElementById(gradeId).style.color='red'; 
 }
 else if(kda>=6){
 document.getElementById(gradeId).innerHTML = 'A+';
-document.getElementById(gradeId).style.color='gold';   
+document.getElementById(gradeId).style.color='#660066';   
 }
 else if(kda>=4.5&&kda<6){
 document.getElementById(gradeId).innerHTML = 'A';
+document.getElementById(gradeId).style.color='green';
 }
 else if(kda>=3&&kda<4.5){
 document.getElementById(gradeId).innerHTML = 'A-';
+document.getElementById(gradeId).style.color='gold';
 }
 else if(kda>=2.5&&kda<3){
-document.getElementById(gradeId).innerHTML = 'B';
-document.getElementById(gradeId).style.color='blue';    
+document.getElementById(gradeId).innerHTML = 'B';   
+document.getElementById(gradeId).style.color='grey'; 
 }
 else if(kda<2.5){
 document.getElementById(gradeId).innerHTML = 'C';
-document.getElementById(gradeId).style.color='bronze';    
+document.getElementById(gradeId).style.color='red';    
 }
+//анализируем голд
+//sup == 9000
+//adc == 12000
+//mid == 12000
+//top == 11500
+//jun == 11000
+
 }
 function commafy( num ) {
     var str = num.toString().split('.');
